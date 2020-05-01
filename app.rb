acc4b535dbbfb20b6eab0be9a2412fbb14f62235
Andrew Hulme <andrewhulme1993@gmail.com>
2020-05-01 10:48:59 +0100

    Redirects from post to get

--- app.rb
+++ app.rb
@@ -5,14 +5,19 @@ require 'shotgun'
 # shotgun ./lib/app.rb -p 4567 (4567 is port)
 
 class Birthday < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
-  post '/birthday' do
-    @name = params[:input_name]
-    @name
+  post '/store_data' do
+    session[:input_name] = params[:input_name]
+    redirect '/birthday'
+  end
+
+  get '/birthday' do
+    session[:input_name]
   end
 
   run! if app_file == $0
